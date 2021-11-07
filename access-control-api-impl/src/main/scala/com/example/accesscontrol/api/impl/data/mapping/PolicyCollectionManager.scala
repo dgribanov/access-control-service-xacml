94bd3386753545e6c87bbf2a8f4361dc9fd26bb2
dgribanov <dg@cm.expert>
2021-11-08 00:14:06 +0300

    actor system new api

--- access-control-api-impl/src/main/scala/com/example/accesscontrol/api/impl/data/mapping/PolicyCollectionManager.scala
+++ access-control-api-impl/src/main/scala/com/example/accesscontrol/api/impl/data/mapping/PolicyCollectionManager.scala
@@ -1,124 +1,124 @@
 package com.example.accesscontrol.api.impl.data.mapping
 
-import akka.actor.Actor
-import akka.actor.ActorRef
-import akka.actor.Props
-import akka.actor.typed.scaladsl.Behaviors
-import akka.event.Logging
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 
-import com.example.accesscontrol.api.impl.domain.{PolicyCollection, PolicySet, Policy, TargetType}
+import com.example.accesscontrol.api.impl.domain.{Policy, PolicyCollection, PolicySet, TargetType}
 
 object PolicyCollectionManager {
-  def props: Props = Props[PolicyCollectionManager]
+  sealed trait Message
 
-  sealed trait Command
-  case class RegistryPolicyCollection(policyCollection: PolicyCollection) extends Command
-  case class RegistryPolicySet(policySet: PolicySet) extends Command
-  case class RegistryPolicy(policy: Policy) extends Command
+  sealed trait Command extends Message
+  final case class RegistryPolicyCollection(policyCollection: PolicyCollection) extends Command
+  final case class RegistryPolicySet(policySet: PolicySet) extends Command
+  final case class RegistryPolicy(policy: Policy) extends Command
 
-  case class FetchPolicyCollection() extends Command
-  case class FetchPolicySet(target: TargetType) extends Command
-  case class FetchPolicy(policySetTarget: TargetType, policyTarget: TargetType) extends Command
-}
-
-class PolicyCollectionManager extends Actor {
-  import PolicyCollectionManager._
-  private val log = Logging.getLogger(context.system, this)
+  sealed trait Query extends Message
+  final case class FetchPolicyCollection(replyTo: ActorRef[Option[PolicyCollection]]) extends Command
+  final case class FetchPolicySet(target: TargetType, replyTo: ActorRef[Option[PolicySet]]) extends Command
+  final case class FetchPolicy(policySetTarget: TargetType, policyTarget: TargetType, replyTo: ActorRef[Option[Policy]]) extends Command
 
   private val policyCollection: Option[PolicyCollection] = None
-  private val policySetMap = Map.empty[String, ActorRef]
+  private val policySetMap = Map.empty[String, ActorRef[Message]]
 
-  override def receive: Receive = onMessage(policyCollection, policySetMap)
+  def apply(): Behavior[Message] = {
+    onMessage(policyCollection, policySetMap)
+  }
 
-  private def onMessage(policyCollection: Option[PolicyCollection], policySetMap: Map[String, ActorRef]): Receive = {
-    case RegistryPolicyCollection(pc) =>
-      log.info("Registry policy collection: {}", pc)
-      val psMap = pc.policySets.map(
-        policySet => policySet.target.value -> spawnChild(policySet)
-      ).toMap
-      context.become(onMessage(Some(pc), psMap))
-    case _: FetchPolicyCollection =>
-      sender ! policyCollection
-    case command @ FetchPolicySet(target) =>
-      policySetMap.get(target.value) match {
-        case Some(policySet) => policySet forward command
-        case None            => sender ! None
-      }
-    case command @ FetchPolicy(target, _) =>
-      policySetMap.get(target.value) match {
-        case Some(policySet) => policySet forward command
-        case None            => sender ! None
+  private def onMessage(policyCollection: Option[PolicyCollection], policySetMap: Map[String, ActorRef[Message]]): Behavior[Message] =
+    Behaviors.receive { (context, message) =>
+      (message: @unchecked) match {
+        case RegistryPolicyCollection(policyCollection) =>
+          context.log.info("Registry policy collection: {}", policyCollection)
+          val policySetMap = policyCollection.policySets.map(
+            policySet => policySet.target.value -> spawnChild(context, policySet)
+          ).toMap
+          onMessage(Some(policyCollection), policySetMap)
+        case FetchPolicyCollection(replyTo) =>
+          replyTo ! policyCollection
+          Behaviors.same
+        case command @ FetchPolicySet(target, replyTo) =>
+          policySetMap.get(target.value) match {
+            case Some(policySet) => policySet ! command
+            case None            => replyTo ! None
+          }
+          Behaviors.same
+        case command @ FetchPolicy(target, _, replyTo) =>
+          policySetMap.get(target.value) match {
+            case Some(policySet) => policySet ! command
+            case None            => replyTo ! None
+          }
+          Behaviors.same
       }
   }
 
-  private def spawnChild(policySet: PolicySet): ActorRef = {
-    val childActorRef = context.actorOf(Props[PolicySetManager], s"policy-set-${policySet.target.value}")
+  private def spawnChild(context: ActorContext[Message], policySet: PolicySet): ActorRef[Message] = {
+    val childActorRef = context.spawn(PolicySetManager(), s"policy-set-${policySet.target.value}")
     childActorRef ! RegistryPolicySet(policySet)
     childActorRef
   }
 }
 
 object PolicySetManager {
-  def props: Props = Props[PolicySetManager]
-}
-
-class PolicySetManager extends Actor {
   import PolicyCollectionManager._
 
-  private val log = Logging.getLogger(context.system, this)
-
   private val policySet: Option[PolicySet] = None
-  private val policyMap = Map.empty[String, ActorRef]
+  private val policyMap = Map.empty[String, ActorRef[Message]]
 
-  override def receive: Receive = onMessage(policySet, policyMap)
-
-  private def onMessage(policySet: Option[PolicySet], policyMap: Map[String, ActorRef]): Receive = {
-    case RegistryPolicySet(ps) =>
-      log.info("Registry policy set: {}", ps)
-      val pMap = ps.policies.map(
-        policy => policy.target.value -> spawnChild(policy)
-      ).toMap
-      context.become(onMessage(Some(ps), pMap))
-    case FetchPolicySet(target) =>
-      policySet match {
-        case Some(ps) if ps.target.value == target.value => sender ! Some(ps)
-        case _                                           => Behaviors.unhandled
-      }
-    case command @ FetchPolicy(_, target) =>
-      policyMap.get(target.value) match {
-        case Some(policy) => policy forward command
-        case None         => Behaviors.unhandled
-      }
+  def apply(): Behavior[Message] = {
+    onMessage(policySet, policyMap)
   }
 
-  private def spawnChild(policy: Policy): ActorRef = {
-    val childActorRef = context.actorOf(Props[PolicyManager], s"policy-${policy.target.value}")
+  private def onMessage(policySet: Option[PolicySet], policyMap: Map[String, ActorRef[Message]]): Behavior[Message] =
+    Behaviors.receive { (context, message) =>
+      (message: @unchecked) match {
+        case RegistryPolicySet(policySet) =>
+          context.log.info("Registry policy set: {}", policySet)
+          val policyMap = policySet.policies.map(
+            policy => policy.target.value -> spawnChild(context, policy)
+          ).toMap
+          onMessage(Some(policySet), policyMap)
+        case FetchPolicySet(target, replyTo) =>
+          policySet match {
+            case Some(ps) if ps.target.value == target.value => replyTo ! Some(ps)
+          }
+          Behaviors.same
+        case command @ FetchPolicy(_, target, _) =>
+          policyMap.get(target.value) match {
+            case Some(policy) => policy ! command
+            case _            =>
+          }
+          Behaviors.same
+      }
+    }
+
+  private def spawnChild(context: ActorContext[Message], policy: Policy): ActorRef[Message] = {
+    val childActorRef = context.spawn(PolicyManager(), s"policy-${policy.target.value}")
     childActorRef ! RegistryPolicy(policy)
     childActorRef
   }
 }
 
 object PolicyManager {
-  def props: Props = Props[PolicyManager]
-}
-
-class PolicyManager extends Actor {
   import PolicyCollectionManager._
-
-  private val log = Logging.getLogger(context.system, this)
 
   private val policy: Option[Policy] = None
 
-  override def receive: Receive = onMessage(policy)
-
-  private def onMessage(policy: Option[Policy]): Receive = {
-    case RegistryPolicy(p) =>
-      log.info("Registry policy: {}", p)
-      context.become(onMessage(Some(p)))
-    case FetchPolicy(_, target) =>
-      policy match {
-        case Some(p) if p.target.value == target.value => sender ! Some(p)
-        case _                                         => Behaviors.unhandled
-      }
+  def apply(): Behavior[Message] = {
+    onMessage(policy)
   }
+
+  private def onMessage(policy: Option[Policy]): Behavior[Message] =
+    Behaviors.receive { (context, message) =>
+      (message: @unchecked) match {
+        case RegistryPolicy(policy) =>
+          context.log.info("Registry policy: {}", policy)
+          onMessage(Some(policy))
+        case FetchPolicy(_, target, replyTo) =>
+          policy match {
+            case Some(p) if p.target.value == target.value => replyTo ! Some(p)
+          }
+          Behaviors.same
+      }
+    }
 }
--- access-control-api-impl/src/main/scala/com/example/accesscontrol/api/impl/data/mapping/PolicyRetrievalPointImpl.scala
+++ access-control-api-impl/src/main/scala/com/example/accesscontrol/api/impl/data/mapping/PolicyRetrievalPointImpl.scala
@@ -4,7 +4,8 @@ import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 
-import akka.actor.ActorSystem
+import akka.actor.typed.{ActorSystem, Scheduler}
+import akka.actor.typed.scaladsl.AskPattern._
 import akka.util.Timeout
 import play.api.libs.json.{JsError, JsSuccess, Json}
 
@@ -24,34 +25,28 @@ import com.example.accesscontrol.api.impl.data.mapping.PolicyCollectionManager.{
 }
 
 final class PolicyRetrievalPointImpl @Inject() (policyRepository: PolicyRepository) extends PolicyRetrievalPoint {
-  private val system = ActorSystem("policy-retrieval-point-system")
-  private val policyCollectionManagerActorRef = system.actorOf(PolicyCollectionManager.props, "policy-collection-manager")
+  private val system: ActorSystem[PolicyCollectionManager.Message] = ActorSystem(PolicyCollectionManager(), "policy-retrieval-point-system")
   buildPolicyCollection()
 
   implicit val ec: ExecutionContext = ExecutionContext.global // don`t move! it`s implicit ExecutionContext for Future
   implicit val timeout: Timeout = Timeout(1.nanosecond) // don`t move! it`s implicit Timeout for Akka ask (?) operator
+  implicit val scheduler: Scheduler = system.scheduler // don`t move! it`s implicit scheduler for Akka ask (?) operator
 
   def fetchPolicyCollection(): Future[Option[PolicyCollection]] = {
-    import akka.pattern.ask
-
-    (policyCollectionManagerActorRef ? FetchPolicyCollection()).mapTo[Option[PolicyCollection]]
+    system ? (ref => FetchPolicyCollection(ref))
   }
 
   def fetchPolicySet(target: TargetType): Future[Option[PolicySet]] = {
-    import akka.pattern.ask
-
-    (policyCollectionManagerActorRef ? FetchPolicySet(target)).mapTo[Option[PolicySet]]
+    system ? (ref => FetchPolicySet(target, ref))
   }
 
   def fetchPolicy(policySetTarget: TargetType, policyTarget: TargetType): Future[Option[Policy]] = {
-    import akka.pattern.ask
-
-    (policyCollectionManagerActorRef ? FetchPolicy(policySetTarget, policyTarget)).mapTo[Option[Policy]]
+    system ? (ref => FetchPolicy(policySetTarget, policyTarget, ref))
   }
 
   def buildPolicyCollection(): Unit = {
       Json.fromJson[PolicyCollectionSerializable](policyRepository.fetchPolicyCollection) match {
-        case JsSuccess(policyCollection, _) => policyCollectionManagerActorRef ! RegistryPolicyCollection(policyCollection)
+        case JsSuccess(policyCollection, _) => system ! RegistryPolicyCollection(policyCollection)
         case JsError.Message(errMsg)        => PolicyCollectionParsingError(errMsg)
       }
   }
